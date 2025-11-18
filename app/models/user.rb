6c81bd536bfbae1f4f6c0053adc4826a3643aa87
Grégory Lefebvre <gregory.lfb1@gmail.com>
2025-11-18 17:19:33 +0100

    model family + model user + migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :family
+  enum status: { member: "member", helper: "helper" }
 end
