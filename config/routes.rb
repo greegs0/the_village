7a8d077b3a0567dd7f80807588eceab2543e9093
fabien maheu <fabienmaheu@gmail.com>
2025-11-19 15:26:56 +0100

    création model event, route et controiller pour index events

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  resources :events, only: :index
   # root "posts#index"
 end
