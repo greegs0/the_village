287234528b2a4f6ebbfb1c6e1813dea56d5bfbd9
fabien maheu <fabienmaheu@gmail.com>
2025-11-19 16:53:15 +0100

    ajouté la vue index events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_19_105406) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_19_121841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,9 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_19_105406) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "events", "users"
-=======
   add_foreign_key "users", "families"
->>>>>>> master
 end
