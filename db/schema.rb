0ea400ffab66f8d6ab9a1f4c9296db8906fa9bdc
Gregory Lefebvre <gregory.lfb1@gmail.com>
2025-11-21 11:06:19 +0100

    CrudPeople and more

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_19_164133) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_21_095414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_19_164133) do
     t.string "status"
     t.bigint "family_id"
     t.string "zipcode"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["family_id"], name: "index_users_on_family_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
