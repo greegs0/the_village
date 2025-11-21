220d52171b29e809554290d66624a67d4c81a409
Gregory Lefebvre <gregory.lfb1@gmail.com>
2025-11-21 11:49:26 +0100

    commit for mergin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_20_155046) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_21_095414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_20_155046) do
     t.string "assigned_to"
     t.string "event_icon"
     t.string "badge_class"
-    t.boolean "reminders_enabled"
+    t.boolean "reminders_enabled", default: false
     t.index ["family_id"], name: "index_family_events_on_family_id"
   end
 
